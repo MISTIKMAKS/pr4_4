14fa07a5b79b7161c1dfe43256b441310aa206d1
MISTIKMAKS <mistikmaks80885@gmail.com>
2020-11-19 02:32:22 +0200

    first master commit

---
+++ pr4_4/pr4_4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, y, xp, xk, dx, R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= 6)
+			y = R;
+		else
+			if (-6 < x && x <= -R)
+				y = -1 + (1 / (-R + 6)) * (x + 6);
+			else
+				if (-R < x && x <= 0)
+					y = -sqrt((-x * x) - (2 * x * R) + (2 * y * R) - R * R);
+				else
+					if (0 < x && x <= R)
+						y = sqrt(R * R - (x - R) * (x - R));
+					else
+						y = (-1 / (1 + R - R)) * (x - R);
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
